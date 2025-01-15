0fcb3fe04a96c19807a7e7b4ba5bd347662cdaa9
SashaTyutyaev <sashatut2002@yandex.ru>
2025-01-15 13:28:01 +0300

    bugfix(STOCK-64) new internal token

--- src/main/java/lissa/trading/tg/bot/tinkoff/feign/InternalTokenFeignInterceptor.java
+++ src/main/java/lissa/trading/tg/bot/tinkoff/feign/InternalTokenFeignInterceptor.java
@@ -5,14 +5,18 @@ import feign.RequestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Component
 public class InternalTokenFeignInterceptor implements RequestInterceptor {
 
-    @Value("${security.internal.token}")
+    @Value("${integration.rest.tinkoff-api-service.token}")
     private String internalToken;
 
     @Override
-    public void apply(RequestTemplate template) {
-        template.header("Authorization", "Bearer " + internalToken);
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header("Authorization",
+                new String(Base64.getDecoder().decode(internalToken), StandardCharsets.UTF_8).trim());
     }
 }
